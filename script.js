d3fba15e5185021fa3bd86880df5c4efb682a602
山谷悠喜 <yamayayuuki@yamataniyuukinoMacBook-Air.local>
2026-04-27 09:16:43 +0900

    トップへ戻るボタンの追加

---
+++ script.js
@@ -0,0 +1,23 @@
+// ハンバーガーメニュー
+    $(function() {
+            $("#drawer_toggle").click(function() {
+            $(this).toggleClass('open');
+            $("#global_nav").slideToggle();
+        });
+    });
+
+// トップへ戻るボタン
+const BottonToTop = document.querySelector(".c-topbtn")
+
+BottonToTop.addEventListener("click", function {
+    window.scroll({ top: 0, behavior: "smooth" });
+});
+
+window.addEventListener("scroll", scroll_event);
+function scroll_event() {
+  if (window.pageYOffset > 100) {
+    pagetop_btn.style.opacity = "1";
+  } else if (window.pageYOffset < 100) {
+    pagetop_btn.style.opacity = "0";
+  }
+}
